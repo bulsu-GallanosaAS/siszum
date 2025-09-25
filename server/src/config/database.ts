c4e9ef613d6ee9f7b9fb768f2d5004ab5de4846c
Andrei S. Gallanosa <2022100907@ms.bulsu.edu.ph>
2025-09-25 21:56:01 +0800

    RAILWAY

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,10 +5,11 @@ dotenv.config();
 
 // Database connection configuration
 const dbConfig = {
-  host: process.env.DB_HOST || 'sql.freedb.tech',
-  user: process.env.DB_USER || 'freedb_siszum_adminn',
-  password: process.env.DB_PASSWORD || '9F!VFT*8rH4pjMF',
-  database: process.env.DB_NAME || 'freedb_siszum_poss',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
